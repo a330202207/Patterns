9ebba1119128709655b6e14633be136959f41d3a
KevinJie <330202207@qq.com>
2017-07-06 06:50:07 +0800

    edit

--- Singleton/index.php
+++ Singleton/index.php
@@ -1,9 +1,20 @@
 <?php
+// 注册自加载
+spl_autoload_register('autoload');
+
+function autoload($class)
+{
+  require dirname($_SERVER['SCRIPT_FILENAME']) . '//..//' . str_replace('\\', '/', $class) . '.php';
+}
+
+
+
+use Singleton\Database;
 
 /**
 *
 * 无论调用多少次，实际上到数据库只有一次连接
 */
-$db = \Singleton\Singleton::getInstance();
-$db = \Singleton\Singleton::getInstance();
-$db = \Singleton\Singleton::getInstance();
+$db = Database::getInstance();
+$db = \Singleton\Database::getInstance();
+$db = \Singleton\Database::getInstance();
