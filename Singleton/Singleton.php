3165fd6a8793578da109793f8adffc559b742a09
a330202207 <330202207@qq.com>
2018-02-22 19:40:08 +0800

    Update Singleton.php

--- Singleton/Singleton.php
+++ Singleton/Singleton.php
@@ -4,54 +4,54 @@ namespace Singleton;
 class Singleton
 {
 
-    /**
-     * 自身实例
-     *
-     * @var object
-     */
+	/**
+	* 自身实例
+	*
+	* @var object
+	*/
 	protected static $_instance;
 
 
-    /**
-     * 构造函数
-     *
-     */
+	/**
+	* 构造函数
+	*
+	*/
 	private function __construct()
 	{
 
-    }
+     	}
 
-    /**
-     * 魔法方法
-     * 禁止clone对象
-     *
-     * @return string
-     */
-    public function __clone()
-    {
-        echo 'clone is forbidden';
-    }
+	/**
+	* 魔法方法
+	* 禁止clone对象
+	*
+	* @return string
+	*/
+	public function __clone()
+    	{
+        	echo 'clone is forbidden';
+    	}
 
-    /**
-     * 获取实例
-     *
-     * @return object
-     */
+	/**
+	* 获取实例
+	*
+	* @return object
+	*/
 	static function getInstance()
 	{
-        if (!self::$_instance instanceof self) {
-            self::$_instance = new self;
-        }
-        return self::$_instance;
+		if (!self::$_instance instanceof self) {
+		    self::$_instance = new self;
+		}
+		return self::$_instance;
 	}
 
-    /**
-     * 测试方法
-     *
-     * @return string
-     */
-    public function test()
-    {
-        echo "这是个测试 \n";
-    }
+	/**
+	* 测试方法
+	*
+	* @return string
+	*/
+	public function test()
+	{
+		echo "这是个测试 \n";
+	}
 }
